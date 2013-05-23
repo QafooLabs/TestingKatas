5627cc1036817b960ff1cfdf4e38d69fc0bcaa7d
Kore Nordmann <mail@kore-nordmann.de>
2013-05-23 15:15:14 +0200

    Added: StringCalculator with first simple tests

---
+++ src/main/Jimdo/StringCalculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jimdo;
+
+class StringCalculator
+{
+    public function add($string)
+    {
+        throw new \RuntimeException("@TODO: Implement");
+    }
+}
---
+++ test/Jimdo/StringCalculatorTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Jimdo;
+
+class StringCalculatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSimpleNumber()
+    {
+        $calculator = new StringCalculator();
+
+        $this->assertEquals(
+            1,
+            $calculator->add("1")
+        );
+    }
+
+    public function testAddTwoNumbers()
+    {
+        $calculator = new StringCalculator();
+
+        $this->assertEquals(
+            6,
+            $calculator->add("2,4")
+        );
+    }
+
+    public function testAddTwoOtherNumbers()
+    {
+        $calculator = new StringCalculator();
+
+        $this->assertEquals(
+            9,
+            $calculator->add("5,4")
+        );
+    }
+
+    public function testAddThreeNumbers()
+    {
+        $calculator = new StringCalculator();
+
+        $this->assertEquals(
+            51,
+            $calculator->add("5,4,42")
+        );
+    }
+}
