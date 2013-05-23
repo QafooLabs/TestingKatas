e5203c7cfc22a4c8995ca84f0f6b18b88a58c215
Kore Nordmann <mail@kore-nordmann.de>
2013-05-23 15:19:52 +0200

    Test for return value for empty string

--- test/Jimdo/StringCalculatorTest.php
+++ test/Jimdo/StringCalculatorTest.php
@@ -4,6 +4,16 @@ namespace Jimdo;
 
 class StringCalculatorTest extends \PHPUnit_Framework_TestCase
 {
+    public function testEmptyString()
+    {
+        $calculator = new StringCalculator();
+
+        $this->assertEquals(
+            0,
+            $calculator->add("")
+        );
+    }
+
     public function testSimpleNumber()
     {
         $calculator = new StringCalculator();
