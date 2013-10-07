b5f1e33604a27fdb72cd2dcbc6d4fea8c665966a
Kore Nordmann <mail@kore-nordmann.de>
2013-10-07 10:22:28 +0200

    Fixed typo

--- src/main/Acme/Verificator/Null.php
+++ src/main/Acme/Verificator/Null.php
@@ -9,7 +9,7 @@ class Null extends Verificator
     public function verify(array $numbers)
     {
         if (count($numbers) > count(array_filter($numbers))) {
-            throw new \InvalidArgumentException("Empty number found ins tirng.");
+            throw new \InvalidArgumentException("Empty number found in string.");
         }
     }
 }
