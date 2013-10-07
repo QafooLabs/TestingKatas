6183eaa2bd731f01b0fac5df0ba7bc6da84b0be1
Kore Nordmann <mail@kore-nordmann.de>
2013-10-07 10:36:35 +0200

    Tested: Verificator is called

--- test/Acme/StringCalculatorTest.php
+++ test/Acme/StringCalculatorTest.php
@@ -25,4 +25,29 @@ class StringCalculatorTest extends \PHPUnit_Framework_TestCase
             $calculator->add("inputstring")
         );
     }
+
+    public function testCallVerificator()
+    {
+        $parser = $this->getMock('\\Acme\\StringCalculator\\Parser');
+        $parser
+            ->expects($this->any())
+            ->method('parse')
+            ->with("inputstring")
+            ->will(
+                $this->returnValue(array(1, 2))
+            );
+
+        $verificator = $this->getMock('\\Acme\\Verificator');
+        $verificator
+            ->expects($this->atLeastOnce())
+            ->method('verify')
+            ->with(array(1, 2));
+
+        $calculator = new StringCalculator(
+            $parser,
+            array($verificator)
+        );
+
+        $calculator->add("inputstring");
+    }
 }
